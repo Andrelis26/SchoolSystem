87c0cb48b6bcf057db8ec37859b257a44df359a5
Andrelis Toledo <andrelistoledo@gmail.com>
2020-04-04 23:45:10 -0400

    Cambio de id en service

--- SchoolSystem/SchoolSystem/Controllers/ServiceController.cs
+++ SchoolSystem/SchoolSystem/Controllers/ServiceController.cs
@@ -24,11 +24,10 @@ namespace SchoolSystem.Controllers
 
         public ActionResult RecordNotas(string PrimerNombre, string PrimerApellido, string SegundoApellido, string NombrePadres, string Year)
         {
-            var Id = from c in db.Calificaciones
-                        select c.Id_Estudiante;
+            var Id =  db.Calificaciones.FirstOrDefault();
 
             var Student = db.Calificaciones.ToList().Where(x => x.Estudiante.Primer_Nombre == PrimerNombre && x.Estudiante.Primer_Apellido == PrimerApellido && x.Estudiante.Segundo_Apelido == SegundoApellido && x.Id_Estudiante == x.Estudiante.ID_Estudiante);
-            var Calification = db.Calificaciones.FirstOrDefault(x => x.Id_Estudiante.ToString() == Id.ToString());
+            var Calification = db.Calificaciones.FirstOrDefault(x => x.Id_Estudiante.ToString() == Id.Id_Estudiante.ToString());
             var Nombre = Calification.Estudiante.Primer_Nombre + " " + Calification.Estudiante.Primer_Apellido + " " + Calification.Estudiante.Segundo_Apelido;
 
             ViewBag.Nombre = Nombre;
